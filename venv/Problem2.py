dad7a5f4488c9d6bc5d46270cc6871a2a99de71a
Laura Bleiel <laura.bleiel@gmail.com>
2018-03-06 10:32:16 -0500

    Initial commit

---
+++ venv/Problem2.py
@@ -0,0 +1,15 @@
+# PROBLEM 2 - How do you interpret the confidence intervals
+# you reported in Problem 1?
+
+# EXPECTED REWARD:
+# If this trial was repeated many times in order to construct a number of
+# independent 95% confidence intervals each based on n observations,
+# the proportion of these confidence intervals that will cover the
+# average expected reward is expected to be 95%.
+
+
+# PROBABILITY OF LOSS:
+# If this trial was repeated many times in order to construct a number of
+# independent 95% confidence intervals each based on n observations,
+# the proportion of these confidence intervals that will cover the
+# average probability of loss is expected to be 95%.
---
+++ venv/Problem3.py
@@ -0,0 +1,26 @@
+# Problem 3 values printed in Problem 1
+
+# PROBLEM 3 ANSWERS:
+# Casino owner:
+# For a casino owner playing the game 1,000 times, the average expected reward for the casino owner is: 25.9
+# For the casino owner, the 95% CI of the casino's average expected reward is: [20.007023505241968, 31.79297649475803]
+
+# If this simulation was repeated many times in order to construct a number of
+# independent 95% confidence intervals each based on 1000 observations,
+# the proportion of these confidence intervals that will cover the
+# average expected reward of the casino owner is expected to be 95%.
+
+
+# Gambler:
+# For a gambler playing the game 10 times, the average expected reward is: -70.0
+# For the gambler, the 95% PI of average expected reward is: [-227.5, 50.0]
+
+# Since there are so few observations (times the gambler is playing the game),
+# the law of large numbers cannot be applied to make an inference about E[X].
+# Therefore, this is a transient-state simulation model and
+# the projection interval must be reported.
+# If this simulation was repeated many times in order to construct a number of
+# independent 95% projection intervals each based on n observations,
+# the proportion of these intervals that will cover the
+# gamblers's average expected reward is expected to be 95%.
+
